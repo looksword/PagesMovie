d921d8c47b533f30040a0040671c287f53e953af
looksword <1003961499@qq.com>
2022-03-08 20:09:54 +0800

    learn asp.net

---
+++ PagesMovie/PagesMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }//主键
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]//指定数据的类型 (Date)
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ PagesMovie/PagesMovie/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PagesMovie.Data;
+using PagesMovie.Models;
+
+namespace PagesMovie.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly PagesMovie.Data.PagesMovieContext _context;
+
+        public DetailsModel(PagesMovie.Data.PagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
